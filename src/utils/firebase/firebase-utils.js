77b95cd63f9db1bb6fbd5439dc0bf479a2eb18cf
SudhanshuSahu <102967661+SudhanshuSahu@users.noreply.github.com>
2023-01-16 16:07:04 +0530

    create document in db using firestore

--- src/utils/firebase/firebase-utils.js
+++ src/utils/firebase/firebase-utils.js
@@ -1,5 +1,9 @@
 import {initializeApp} from 'firebase/app'
 import {getAuth,signInWithRedirect, signInWithPopup , GoogleAuthProvider} from 'firebase/auth'
+import {getFirestore,doc,getDoc,setDoc} from 'firebase/firestore'
+
+
+
 const firebaseConfig = {
   apiKey: "AIzaSyCPJ8VW26Y2n7kJureJpBzlGXB4UcKDiTo",
   authDomain: "crwn-clothing-db-2ead2.firebaseapp.com",
@@ -19,3 +23,26 @@ provider.setCustomParameters({
 export const auth = getAuth();
 
 export const signInWithGooglePopup = ()=> signInWithPopup(auth,provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth)=>{
+ const userDocRef = doc(db, "users",userAuth.uid)
+
+ const userSnapShot = await getDoc(userDocRef)
+
+ console.log(userSnapShot);
+
+ if(!userSnapShot.exists()){
+  const {displayName , email} = userAuth;
+  const createdAt = new Date();
+  
+  try {
+    await setDoc(userDocRef,{
+      displayName,email,createdAt
+    })
+  } catch (error) {
+    console.log('error creating the user',error.message);
+  }
+ }
+}
